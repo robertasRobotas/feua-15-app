a4c3072cfa203f2800b63cd1059848661770a799
Robertas Ankudovicius <robertas.ankudovicius@twoday.com>
2024-09-17 18:34:10 +0300

    Changed server url

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    SERVER_URL: "http://localhost:3002",
+    // SERVER_URL: "http://localhost:3002",
+    SERVER_URL: "https://inventory-api-7y7n.onrender.com",
     JWT_KEY: "inventory_app_jwt",
   },
 };
